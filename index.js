4589844de018f8a9217e4cc51bd24efa7cac79ad
Ben <menubar@protonmail.com>
2018-01-31 23:15:53 -0500

    :pencil2: patch

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ const set = require('lodash.set')
 const appRoot = require('app-root-path').path
 const pathToGit = require('path').join(appRoot, '.git')
 
-async function isDirty() {
+async function isDirty () {
   try {
     await execa('git', ['diff-index', '--quiet', 'HEAD', '--'])
-    return false;
-  } catch(err) {
-    return true;
+    return false
+  } catch (err) {
+    return true
   }
 }
 
-async function hasGit() {
+async function hasGit () {
   try {
     await pathExists(pathToGit)
-    return true;
+    return true
   } catch (err) {
-    return false;
+    return false
   }
 }
 
-async function getConfig() {
+async function getConfig () {
   try {
     let remotes = await execa('git', ['config', '--list'])
 
     return remotes.stdout.split(/[\r\n]+/g)
-      .reduce((acc, val) => 
+      .reduce((acc, val) =>
         set(acc, val.split('=')[0], val.split('=')[1]), {}
       )
-  } catch(err) {
+  } catch (err) {
     return undefined
   }
 }
